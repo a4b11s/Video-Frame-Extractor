a73547b8f6408cf8de3825ba7b29a39be4fd2df3
Artem <a4b11s@gmail.com>
2024-07-21 17:49:38 +0300

    feat: Add option to compress frames during extraction

--- v2p/video_framer.py
+++ v2p/video_framer.py
@@ -1,19 +1,30 @@
 import os
 import cv2
 import tqdm
+import zipfile
 
 
 class VideoFramer(object):
-    def __init__(self, video_path, frame_dir, frame_rate=1, verbose=False):
+    def __init__(
+        self,
+        video_path,
+        frame_dir,
+        frame_rate=1,
+        verbose=False,
+        compress=False,
+        compress_level=9,
+    ):
         self.video_path = video_path
         self.frame_dir = frame_dir
         self.frame_rate = frame_rate
+        self.compress = compress
+        self.compress_level = compress_level
 
         if not os.path.exists(self.video_path):
             raise FileNotFoundError(f"Video file not found: {self.video_path}")
 
         self._frame_dir()
-        
+
         self.verbose = verbose
 
     def extract_frames(self):
@@ -36,10 +47,25 @@ class VideoFramer(object):
     def _extract_frame(self, frame, frame_count):
         self._on_frame_start(frame_count)
 
+        if self.compress:
+            self._compress_frame(frame, frame_count)
+        else:
+            self._save_frame(frame, frame_count)
+
+        self._on_frame_end(frame_count)
+
+    def _save_frame(self, frame, frame_count):
         frame_path = os.path.join(self.frame_dir, f"{frame_count}.jpg")
         cv2.imwrite(frame_path, frame)
 
-        self._on_frame_end(frame_count)
+    def _compress_frame(self, frame, frame_count):
+        file_name = f"{frame_count}.jpg"
+        self.zip_file.writestr(
+            file_name,
+            cv2.imencode(".jpg", frame)[1].tobytes(),
+            compress_type=zipfile.ZIP_DEFLATED,
+            compresslevel=self.compress_level,
+        )
 
     def _frame_dir(self):
         if not os.path.exists(self.frame_dir):
@@ -57,8 +83,18 @@ class VideoFramer(object):
         if self.verbose:
             self.pbar = tqdm.tqdm(total=self._frame_count(), unit="frames")
 
+        if self.compress:
+            zip_file_path = os.path.join(self.frame_dir, "frames.zip")
+            self.zip_file = zipfile.ZipFile(
+                zip_file_path,
+                "w",
+                compression=zipfile.ZIP_DEFLATED,
+                compresslevel=self.compress_level,
+            )
+
     def _on_extracting_end(self):
-        pass
+        if hasattr(self, "zip_file"):
+            self.zip_file.close()
 
     def _on_frame_start(self, frame_count):
         pass
@@ -68,7 +104,7 @@ class VideoFramer(object):
 
     def _on_iteration_start(self):
         pass
-    
+
     def _on_iteration_end(self):
         if hasattr(self, "pbar") and self.verbose:
             self.pbar.update(1)
